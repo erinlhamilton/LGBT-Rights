50041a70796c3bd59222658be49fc77724b3dcc4
Erin <erin@erinhamilton.me>
2013-04-20 12:52:25 -0500

    Basic File Structure Created

---
+++ js/chart.js
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////
+// Main Class File:   main.js
+// File:              chart.js
+//
+// Author:           (your name and email address)
+//
+// Description:     (Succint description of this file here)
+//                   
+// Credits:          (list anyone who helped you write your program)
+//////////////////////////// 80 columns wide //////////////////////////////////
+
+/* Global Variables*/
+
+
+
+/**
+ * Use this comment for any function you write
+ * (Write a succinct description of this method here.  If necessary,
+ * additional paragraphs should be preceded by <p>, the html tag for
+ * a new paragraph.)
+ *
+ * @param (parameter name) (Describe the first parameter here)
+ * @param (parameter name) (Do the same for each additional parameter)
+ * @return (description of the return value)
+ */
---
+++ js/main.js
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////
+// Title:           LGBT Civil Rights History
+// Source Files:    d3.v3.min.js (Data Driven Documents JavaScript library V3),
+// 					index.html, style.css, map.js, timeline.js, & chart.js
+//
+// Author:          Erin Hamilton
+// Email:           erin@erinhamilton.me
+// Author:          Rashauna Mead
+// Email:           rashauna.mead@gmail.com
+// Author:          Vanessa Knoppke-Wetzel
+// Email:           run.for.funner@gmail.com
+//
+// Description:     (Succint description of the purpose of this file here)
+//                   
+// Credits:          (list anyone who helped you write your program)
+//////////////////////////// 80 columns wide //////////////////////////////////
+
+/* Global Variables */
+
+/**
+ * (Write a succinct description of this method here.  If necessary,
+ * additional paragraphs should be preceded by <p>, the html tag for
+ * a new paragraph.)
+ *
+ * @param (parameter name) (Describe the first parameter here)
+ * @param (parameter name) (Do the same for each additional parameter)
+ * @return (description of the return value)
+ */
